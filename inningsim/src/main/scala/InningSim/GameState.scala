4278667091d8ff47be0c89fcccac504b52f3063b
Ben Dilday <ben.dilday.phd@gmail.com>
2019-02-26 20:55:47 -0500

    wip

--- inningsim/src/main/scala/InningSim/GameState.scala
+++ inningsim/src/main/scala/InningSim/GameState.scala
@@ -21,25 +21,22 @@ case class BaseOutState(bases: BaseState = BaseState(Map(1 -> 0, 2 -> 0, 3 -> 0)
 
 }
 
-case class GameState(base_outs: BaseOutState = BaseOutState(), score: Array[Int] = Array(0, 0)) extends EvolveState {
+case class GameState(base_outs: BaseOutState = BaseOutState(), score: Array[Int] = Array(0, 0)) {
 
   override def toString: String = {
     s"base out: $base_outs score ${score(0)} to ${score(1)}"
   }
-}
 
-trait EvolveState {
-  self: GameState =>
   def evolve_state(ev: BatEvent): GameState = {
-    val b1 = self.base_outs.bases.bases.get(1).get
-    val b2 = self.base_outs.bases.bases.get(2).get
-    val b3 = self.base_outs.bases.bases.get(3).get
+    val b1 = this.base_outs.bases.bases.get(1).get
+    val b2 = this.base_outs.bases.bases.get(2).get
+    val b3 = this.base_outs.bases.bases.get(3).get
 
     val bo = ev match {
       case xout() => {
         BaseOutState(
-          self.base_outs.bases,
-          self.base_outs.outs + 1
+          this.base_outs.bases,
+          this.base_outs.outs + 1
         )
       }
       case xbb() => {
@@ -47,40 +44,40 @@ trait EvolveState {
         val x2: Int = if (b1 == 1) 1 else b2
         val x3: Int = if (b1 == 1 && b2 == 1) 1 else b3
         BaseOutState(
-        BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), self.base_outs.outs)
+          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), this.base_outs.outs)
       }
       case x1b() => {
         val x1 = 1
         val x2: Int = if (b1 == 1) 1 else 0
         val x3: Int = if (b2 == 1) 1 else 0
         BaseOutState(
-          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), self.base_outs.outs)
+          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), this.base_outs.outs)
       }
       case x2b() => {
         val x1 = 0
         val x2: Int = 1
         val x3: Int = if (b1 == 1) 1 else 0
         BaseOutState(
-          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), self.base_outs.outs)
+          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), this.base_outs.outs)
       }
       case x3b() => {
         val x1 = 0
         val x2: Int = 0
         val x3: Int = 1
         BaseOutState(
-          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), self.base_outs.outs)
+          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), this.base_outs.outs)
       }
       case x4b() => {
         val x1 = 0
         val x2: Int = 0
         val x3: Int = 0
         BaseOutState(
-          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), self.base_outs.outs)
+          BaseState(Map(1 -> x1, 2 -> x2, 3 -> x3)), this.base_outs.outs)
       }
     }
-    val delta_runs = BatEvents.runs_scored(self.base_outs, bo)
-    val new_score = Array(self.score(0) + delta_runs, self.score(1))
+    val delta_runs = BatEvents.runs_scored(this.base_outs, bo)
+    val new_score = Array(this.score(0) + delta_runs, this.score(1))
     GameState(bo, new_score)
   }
-}
 
+}
