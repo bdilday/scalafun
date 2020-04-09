bf36a4c20b25793927c91f40069fd03a8b05e64f
Ben Dilday <ben.dilday.phd@gmail.com>
2020-04-09 11:18:58 -0400

    comments out failing test

--- ScalaLeetCode/src/main/scala/com/leet/degreeofanarray/DegreeOfAnArray.scala
+++ ScalaLeetCode/src/main/scala/com/leet/degreeofanarray/DegreeOfAnArray.scala
@@ -5,16 +5,17 @@ package com.leet.degreeofanarray
 object DegreeOfAnArray {
 
   def findShortestSubArray(nums: Array[Int]): Int = {
+    1
 
   }
 
-  def update_map(indexed_nums: Array[(Int, Int)], result: Map[Int, Int]): Map[Int, Int] = {
-    indexed_nums match {
-      case x if x.isEmpty => result
-      case _ => {
-        val nm = Map()
-        update_map(indexed_nums.tail)
-      }
-    }
-  }
+//  def update_map(indexed_nums: Array[(Int, Int)], result: Map[Int, Int]): Map[Int, Int] = {
+//    indexed_nums match {
+//      case x if x.isEmpty => result
+//      case _ => {
+//        val nm = Map()
+//        update_map(indexed_nums.tail)
+//      }
+//    }
+//  }
 }
